96b1bc2fe35d8233dcb28936a604b1599876ba62
ym1111-ym <isabelyiming@gmail.com>
2020-10-08 13:02:46 -0700

    update wifi screens

--- screens/wifi/wifi-12.js
+++ screens/wifi/wifi-12.js
@@ -1,7 +1,125 @@
-import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { Component } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Wifi13 from "./wifi-13.js";
+import Wifi14 from "./wifi-14.js";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 
 const Wifi12 = ({ navigation }) => {
-  return <View></View>;
+  const textToSpeech =
+    "To check if you have connect successfully, you can swipe down again, and check if you can see this";
+  return (
+    <SafeAreaView style={styles.outerContainer}>
+      <Header></Header>
+      <View style={styles.container}>
+        <Text style={styles.text}>{textToSpeech}</Text>
+        <Image
+          style={styles.image1}
+          source={require("../../assets/wifi_success.png")}
+        />
+      </View>
+      <View style={styles.buttonView}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Wifi13")}
+          style={styles.YesButtonContainer}
+        >
+          <Text style={styles.YesButtonText}>Yes</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Wifi14")}
+          style={styles.NoButtonContainer}
+        >
+          <Text style={styles.NoButtonText}>No</Text>
+        </TouchableOpacity>
+      </View>
+      <Footer></Footer>
+    </SafeAreaView>
+  );
 };
+
 export default Wifi12;
+// <Image source={require("./speaker.png")} />
+const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    position: "relative",
+  },
+  container: {
+    flexDirection: "row",
+    width: "90%",
+    left: "4%",
+    flex: 1,
+    bottom: "10%",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+
+  image1: {
+    alignItems: "center",
+    position: "absolute",
+    top: "80%",
+    left: "10%",
+    width: 400,
+    height: 300,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 40,
+  },
+  speaker: {
+    position: "relative",
+    bottom: "5%",
+  },
+  buttonView: {
+    width: "100%",
+    flex: 1,
+    position: "relative",
+    flexDirection: "row",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: "2%",
+  },
+  YesButtonContainer: {
+    top: "15%",
+    width: "20%",
+    borderWidth: 3,
+    borderRadius: 20,
+    borderColor: "black",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "2%",
+  },
+  YesButtonText: {
+    fontSize: 40,
+  },
+  NoButtonContainer: {
+    top: "15%",
+    width: "20%",
+    borderWidth: 3,
+    borderRadius: 20,
+    borderColor: "black",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "2%",
+  },
+  NoButtonText: {
+    fontSize: 40,
+  },
+});
--- screens/wifi/wifi-13.js
+++ screens/wifi/wifi-13.js
@@ -1,7 +1,102 @@
-import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { Component } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import WelcomeTutorials from "../home/WelcomeTutorials.js";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 
 const Wifi13 = ({ navigation }) => {
-  return <View></View>;
+  const textToSpeech = "Congratulation! You are done with setting up the WiFi!";
+  return (
+    <SafeAreaView style={styles.outerContainer}>
+      <Header></Header>
+      <View style={styles.container}>
+        <Text style={styles.text}>{textToSpeech}</Text>
+        <Image
+          style={styles.image1}
+          source={require("../../assets/wifi_ok.jpg")}
+        />
+      </View>
+      <View style={styles.buttonView}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("WelcomeTutorials")}
+          style={styles.YesButtonContainer}
+        >
+          <Text style={styles.YesButtonText}>Go back to home page</Text>
+        </TouchableOpacity>
+      </View>
+      <Footer></Footer>
+    </SafeAreaView>
+  );
 };
+
 export default Wifi13;
+// <Image source={require("./speaker.png")} />
+const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    position: "relative",
+  },
+  container: {
+    flexDirection: "row",
+    width: "90%",
+    left: "4%",
+    flex: 1,
+    bottom: "10%",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  image1: {
+    alignItems: "center",
+    position: "absolute",
+    top: "80%",
+    left: "21%",
+    width: 300,
+    height: 260,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 40,
+  },
+  speaker: {
+    position: "relative",
+    bottom: "5%",
+  },
+  buttonView: {
+    width: "100%",
+    flex: 1,
+    position: "relative",
+    flexDirection: "row",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: "2%",
+  },
+  YesButtonContainer: {
+    //top: "25%",
+    width: "40%",
+    borderWidth: 3,
+    borderRadius: 20,
+    borderColor: "black",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "2%",
+  },
+  YesButtonText: {
+    fontSize: 40,
+  },
+});
